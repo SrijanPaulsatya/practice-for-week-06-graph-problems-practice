9b14f23c13ff81ebdd1a10dd248e5d67f0c58d85
Srijan Paulsatya <srijanpaulsatya@gmail.com>
2025-01-15 23:23:56 +0530

    Add getNeighbors Function

--- island.js
+++ island.js
@@ -11,6 +11,26 @@ function getNeighbors(row, col, graph) {
   // Return neighbors
 
   // Your code here
+  let neighbors = [];
+
+  if (row !== 0 && graph[row - 1][col] !== 0) {
+    neighbors.push([row - 1, col]);
+  }
+
+  if (row !== graph.length - 1 && graph[row + 1][col] !== 0) {
+    neighbors.push([row + 1, col]);
+  }
+
+  if (col !== 0 && graph[row][col - 1] !== 0) {
+    neighbors.push([row, col - 1]);
+  }
+
+  if (col !== (graph[0].length - 1) && graph[row][col + 1] !== 0) {
+    neighbors.push([row, col + 1]);
+  }
+
+  return neighbors;
+    
 }
 
 
