b9b1428d2739d6a0f63609680d529c0be8c6597e
Srijan Paulsatya <srijanpaulsatya@gmail.com>
2025-01-16 21:45:57 +0530

    Add islandSize Function

--- island.js
+++ island.js
@@ -37,25 +37,45 @@ function getNeighbors(row, col, graph) {
 function islandSize(row, col, graph) {
 
   // Create a visited set to store visited nodes
+  let visitedIslands = new Set();
 
   // Create a stack, put the starting node in the stack
+  let stack = [[row,col]];
 
   // Put the stringified starting node in visited
+  visitedIslands.add([row,col].join(','));
 
   // Initialize size to 0
+  let islandSize = 0;
 
   // While the stack is not empty,
+  while (stack.length > 0) {
 
     // Pop the first node
+    let currentIsland = stack.pop();
+    let [row, col] = currentIsland;
 
     // DO THE THING (increment size by 1)
+    islandSize += 1;
 
     // Then push all the UNVISITED neighbors on top of the stack
     // and mark them as visited
     // HINT: This is what your helper function `getNeighbors` is for
     // HINT: Remember, you're storing your visited nodes as strings!
 
+    let neighbors = getNeighbors(row, col, graph);
+
+    for (let neighbor of neighbors) {
+      let neighborStr = neighbor.join(",");
+      if (!visitedIslands.has(neighborStr)) {
+        visitedIslands.add(neighborStr);
+        stack.push(neighbor);
+      }
+    }
+  }
+
   // return size
+  return islandSize;
 
   // Your code here
 }
